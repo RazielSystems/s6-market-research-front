d2457707b8d5a0d60aa63d888e53c4d3a88f6cb6
Miguel Angel Portillo Bobadilla <gemeloportillo@yahoo.com>
2025-02-10 19:13:21 -0600

    estilos cards sanciones

--- src/app/data-sancion-empresa/data-sancion-empresa.component.ts
+++ src/app/data-sancion-empresa/data-sancion-empresa.component.ts
@@ -1,10 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-data-sancion-empresa',
   templateUrl: './data-sancion-empresa.component.html',
-  styleUrls: ['./data-sancion-empresa.component.css'],
+  styleUrls: ['./data-sancion-empresa.component.css']
 })
 export class DataSancionEmpresaComponent {
-  @Input() sanciones: any = [];
+  @Input() sanciones: any[] = [];
+  @Input() showInDetails: boolean = false;
+  selectedSancionIndex: number = 0;
+
+  get selectedSancion() {
+    return this.sanciones[this.selectedSancionIndex];
+  }
+
+  selectSancion(index: number) {
+    this.selectedSancionIndex = index;
+  }
 }
