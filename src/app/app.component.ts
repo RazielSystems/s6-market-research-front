34360329d9db5fbcf012d879800ca7515791e7a5
Miguel Angel Portillo Bobadilla <gemeloportillo@yahoo.com>
2025-02-11 19:35:25 -0600

    update styles sanciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,16 @@ export class AppComponent implements OnInit {
   showNoResults: boolean = false;
   query: string = '';
   resultados: Array<any> = [];
-  //url: string = 'http://172.20.30.161:4100/api/search';
-  //url: string = 'http://localhost:4100/api/search';
   url: string = 'https://dev-estudio-mercado.plataformadigitalnacional.org/back/api/search';
   
   infoDetalles: any | undefined;
   infoEFO: any | undefined;
-  infoSP: any[] = []; // Cambiado a array
+  infoSP: any[] = [];
   infoSanciones: any | undefined;
-  selectedServidor: any = null; // Para guardar el servidor seleccionado
+  selectedServidor: any = null;
+  
+  selectedButton: string = 'general';
+  showServidoresList: boolean = true;
   
   showGeneralInfo: boolean = true;
   showEfosInfo: boolean = false;
@@ -35,7 +36,6 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    // Obtén la referencia al modal usando su ID
     const modalElement = document.getElementById('modalEFOS');
     const modalElementSP = document.getElementById('modalSP');
     const modalSanciones = document.getElementById('modalSanciones');
@@ -74,7 +74,6 @@ export class AppComponent implements OnInit {
       this.show_loading = false;
       console.log('this.resultados: ', this.resultados);
       
-      // Muestra el mensaje de no resultados si el arreglo está vacío
       if (this.resultados.length === 0) {
         this.showNoResults = true;
       }
@@ -102,7 +101,7 @@ export class AppComponent implements OnInit {
 
   openModalSP(sp: any[]) {
     if (this.modalInstanceSP) {
-      this.infoSP = sp;  // Recibe un array de servidores
+      this.infoSP = sp;
       this.modalInstanceSP.show();
     }
   }
@@ -110,7 +109,7 @@ export class AppComponent implements OnInit {
   closeModalSP() {
     if (this.modalInstanceSP) {
       this.modalInstanceSP.hide();
-      this.selectedServidor = null; // Limpiar la selección
+      this.selectedServidor = null;
       this.infoSP = [];
     }
   }
@@ -131,11 +130,14 @@ export class AppComponent implements OnInit {
   openModalDetalles(data: any) {
     if (this.modalDetalles) {
       this.infoDetalles = data;
-      // Reseteamos el estado de los toggles al abrir el modal
+      // Estado inicial del modal
+      this.selectedButton = 'general';
       this.showGeneralInfo = true;
       this.showEfosInfo = false;
       this.showServidoresInfo = false;
       this.showSancionesInfo = false;
+      this.showServidoresList = true; // La lista comienza visible
+      this.selectedServidor = null;
       this.modalDetalles.show();
     }
   }
@@ -143,52 +145,55 @@ export class AppComponent implements OnInit {
   closeModalDetalles() {
     if (this.modalDetalles) {
       this.modalDetalles.hide();
-      // Reseteamos el estado de los toggles al cerrar el modal
+      // Reseteamos al estado inicial
+      this.selectedButton = 'general';
       this.showGeneralInfo = true;
       this.showEfosInfo = false;
       this.showServidoresInfo = false;
       this.showSancionesInfo = false;
+      this.showServidoresList = false;
+      this.selectedServidor = null;
     }
   }
 
-  toggleGeneralInfo() {
-    this.showGeneralInfo = !this.showGeneralInfo;
-    this.showEfosInfo = false;
-    this.showServidoresInfo = false;
-    this.showSancionesInfo = false;
+  // Método para controlar solo la visibilidad de la lista
+  toggleServidoresList() {
+    this.showServidoresList = !this.showServidoresList;
   }
 
-  toggleEfosInfo() {
-    this.showEfosInfo = !this.showEfosInfo;
-    this.showGeneralInfo = false;
-    this.showServidoresInfo = false;
-    this.showSancionesInfo = false;
-  }
-
-  toggleServidoresInfo(servidor?: any) {
-    this.showServidoresInfo = !this.showServidoresInfo;
-    this.showGeneralInfo = false;
+  selectButton(buttonType: string) {
+    this.selectedButton = buttonType;
+    
+    // Resetear estados
     this.showEfosInfo = false;
     this.showSancionesInfo = false;
     
-    if (servidor) {
-      this.selectedServidor = servidor;
-    } else if (!this.showServidoresInfo) {
-      this.selectedServidor = null;
-    } else if (!this.selectedServidor && this.infoDetalles?.servidores_publicos?.length) {
-      this.selectedServidor = this.infoDetalles.servidores_publicos[0];
+    switch(buttonType) {
+      case 'general':
+        this.showGeneralInfo = true;
+        this.showServidoresInfo = false;
+        this.selectedServidor = null;
+        break;
+      case 'efos':
+        this.showGeneralInfo = false;
+        this.showServidoresInfo = false;
+        this.showEfosInfo = true;
+        this.selectedServidor = null;
+        break;
+      case 'sanciones':
+        this.showGeneralInfo = false;
+        this.showServidoresInfo = false;
+        this.showSancionesInfo = true;
+        this.selectedServidor = null;
+        break;
     }
-  }
-
-  toggleSancionesInfo() {
-    this.showSancionesInfo = !this.showSancionesInfo;
-    this.showGeneralInfo = false;
-    this.showEfosInfo = false;
-    this.showServidoresInfo = false;
   }
 
   selectServidor(servidor: any) {
     this.selectedServidor = servidor;
+    this.showGeneralInfo = false;
+    this.showServidoresInfo = true;
+    this.showEfosInfo = false;
+    this.showSancionesInfo = false;
   }
-  
 }
